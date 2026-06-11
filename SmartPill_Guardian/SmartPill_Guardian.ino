0d52cddb4797b20632249d2f878abd461b71fc20
Lau Yik Chun Isaac <94112290+isaaclau888@users.noreply.github.com>
2026-06-11 12:26:28 +0800

    Add files via upload

--- SmartPill_Guardian/SmartPill_Guardian.ino
+++ SmartPill_Guardian/SmartPill_Guardian.ino
@@ -1,16 +1,21 @@
 #include <Wire.h>
 #include <RTClib.h>
 #include <MCUFRIEND_kbv.h>
-#include <Adafruit.GFX.h>
+#include <Adafruit_GFX.h>
 #include <TouchScreen.h>
 
-#define STEPPER_IN1 22
-#define STEPPER_IN2 23
-#define STEPPER_IN3 24
-#define STEPPER_IN4 25
-#define IR_PILL 10
-#define IR_CUP 11
-#define BUZZER 12
+#define TRIG_PIN_CUP 22
+#define ECHO_PIN_CUP 23
+
+#define TRIG_PIN_DROP 24
+#define ECHO_PIN_DROP 25
+
+#define STEPPER_IN1 26
+#define STEPPER_IN2 27
+#define STEPPER_IN3 28
+#define STEPPER_IN4 29
+
+#define BUZZER 30
 
 #define YP A1 //Touch Y+
 #define XM A2 //Touch X-
@@ -25,16 +30,29 @@ TouchScreen ts = TouchScreen(XP, YP, XM, YM, 300);
 int remainingPills[6] = {42, 35, 28, 40, 30, 25};
 int dailyDose[6] = {2, 2, 1, 2, 1, 2};
 
-unsigned long lastupdate = 0;
+unsigned long lastUpdate = 0;
+
+void drawHomeScreen();
+void updateTimeDisplay();
+void checkLowStock();
+void dispenseMedication(int compartment, int quantity);
+void rotateToCompartment(int comp);
+void stepMotor(long steps);
+void showWarning(String msg);
+void triggerReminder();
+long readUltrasonicDistance(int trigPin, int echoPin);
 
 void setup() {
   Serial.begin(9600);
 
-  pinMode(BUZZER, OUTPUT);
-  pinMode(IR_PILL, INPUT_PULLUP);
-  pinMode(IR_CUP, INPUT_PULLUP);
+  pinMode(TRIG_PIN_CUP, OUTPUT);
+  pinMode(ECHO_PIN_CUP, INPUT);
+  pinMode(TRIG_PIN_DROP, OUTPUT);
+  pinMode(ECHO_PIN_DROP, INPUT);
 
-  for(int i=22; i<=25; i++) pinMode(i, OUTPUT);
+  for(int i=22; i<=25; i++) {
+    pinMode(i, OUTPUT); 
+  }
 
   uint16_t ID = tft.readID();
   tft.begin(ID);
@@ -54,25 +72,24 @@ void loop() {
     updateTimeDisplay();
     checkLowStock();
     lastUpdate = millis();
-    }
-
-    DateTime now = rtc.now();
-    if ((now.hour() == 8 || now.hour() == 14 || now.hour() == 20) && now.minute() == 0) {
-      dispenseMedication(0, 2);
-    }
-
-    TSPoint p = ts.getpoint();
-    if (p.z > 20) {
-      tone(BUZZER, 1000, 100);
-    }
-
-    delay(50);
   }
+
+  DateTime now = rtc.now();
+  if ((now.hour() == 8 || now.hour() == 14 || now.hour() == 20) && now.minute() == 0) {
+    dispenseMedication(0, 2);
+  }
+
+  TSPoint p = ts.getPoint();
+  if (p.z > 20) {
+    tone(BUZZER, 1000, 100);
+  }
+
+  delay(50);
 }
 
 void drawHomeScreen() {
   tft.fillScreen(0x0000);
-  tft.serTextColor(0xFFFF;
+  tft.setTextColor(0xFFFF;
   tft.setTextSize(3);
   tft.setCursor(20, 20);
   tft.println("SmartPill");
@@ -82,7 +99,7 @@ void drawHomeScreen() {
 
 void updateTimeDisplay() {
   DateTime now = rtc.now();
-  tft.fillRect(20. 120, 250, 40, 0x0000);
+  tft.fillRect(20, 120, 250, 40, 0x0000);
 
   tft.setTextSize(4);
   tft.setTextColor(0x07E0);
@@ -103,14 +120,31 @@ void showWarning(String msg) {
 void dispenseMedication(int compartment, int quantity) {
   showWarning("Dispensing...");
 
+  long cupDistance = readUltrasonicDistance(TRIG_PIN_CUP, ECHO_PIN_CUP);
+  if (cupDistance > 10) {
+    showWarning("Error: Place Cup!");
+    triggerReminder();
+    return;
+  }
+
   rotateToCompartment(compartment);
 
   int count = 0;
+  unsigned long startWait = millis();
+
   while (count < quantity) {
     stepMotor(40);
-    if (digitalRead(IR_PILL) == LOW) {
+
+    long dropDistance = readUltrasonicDistance(TRIG_PIN_DROP, ECHO_PIN_DROP);
+    if (dropDistance < 5) {
       count++;
       delay(300);
+    }
+
+    if (millis() - startWait > 15000) {
+      showWarning("Dispenser Jammed!");
+      triggerReminder();
+      return;
     }
   }
 
@@ -153,4 +187,16 @@ void checkLowStock() {
       return;
     }
   }
+}
+
+long readUltrasonicDistance(int trigPin, int echoPin) {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  long duration = pulseIn(echoPin, HIGH, 30000);
+  long distance = duration * 0.034 / 2;
+  return (distance == 0) ? 999 : distance;
 }
